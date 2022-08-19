7f420d506252d1bef7b70a767ab2724f2187e7ad
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2022-08-19 08:37:53 +0600

    case section aded

--- js/case.js
+++ js/case.js
@@ -1,10 +1,11 @@
 
-
+// total case prices
 function updateCaseNumber(isIncrease) {
+
     const caseNumber = document.getElementById('case-number-field');
     const caseNumberString = caseNumber.value;
-    let newCaseNumber;
     const previousCaseNumber = parseInt(caseNumberString);
+    let newCaseNumber;
     if (isIncrease === true) {
         newCaseNumber = previousCaseNumber + 1;
         caseNumber.value = newCaseNumber;
@@ -13,16 +14,21 @@ function updateCaseNumber(isIncrease) {
         newCaseNumber = previousCaseNumber - 1;
         caseNumber.value = newCaseNumber;
     }
+
     const balance = newCaseNumber * 59;
     const updateBalance = document.getElementById('case-balance-field');
     updateBalance.innerText = balance;
-
+    return balance;
 }
 
 document.getElementById('case-btn-plus').addEventListener('click', function () {
     updateCaseNumber(true);
 
+
+
 })
 document.getElementById('case-btn-minus').addEventListener('click', function () {
     updateCaseNumber(false);
 })
+
+// subtotal
